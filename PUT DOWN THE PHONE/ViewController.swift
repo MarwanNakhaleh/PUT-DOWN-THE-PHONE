47bb0138cd623ee6b98216140c415239b15c8854
Marwan Nakhaleh <mnakhaleh@gmail.com>
2025-05-30 06:48:27 -0400

    Initial Commit

---
+++ PUT DOWN THE PHONE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PUT DOWN THE PHONE
+//
+//  Created by Marwan Nakhaleh on 5/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PUT DOWN THE PHONEUITests/PUT_DOWN_THE_PHONEUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PUT_DOWN_THE_PHONEUITestsLaunchTests.swift
+//  PUT DOWN THE PHONEUITests
+//
+//  Created by Marwan Nakhaleh on 5/30/25.
+//
+
+import XCTest
+
+final class PUT_DOWN_THE_PHONEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
